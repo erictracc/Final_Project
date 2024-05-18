f9164e82bb7c884a5024b289fb46481fe6949d31
Eric <erictraccitto@gmail.com>
2024-05-17 22:22:56 -0400

    Setup the database connection to phpmyadmin and created the login page.

--- config/util.php
+++ config/util.php
@@ -1,1 +1,34 @@
 <?php
+
+// Database connection parameters
+$serverName = "localhost";
+$username = "root";
+$password = "";
+$dbname = "final_project";
+
+// Establishing database connection
+$conn = new mysqli($serverName, $username, $password, $dbname);
+
+// Check if connection was successful
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+// Function to generate password hash
+function setPasswordHash($pass): string
+{
+    return password_hash($pass, PASSWORD_DEFAULT);
+}
+
+// Function to generate success message
+function success($text): string
+{
+    return "<div style='text-align:center;padding-bottom:5px;color:green;'>$text</div>";
+}
+
+// Function to generate failure message
+function fail($text): string
+{
+    return "<div style='text-align:center;padding-bottom:5px;color:#FF0019;'>$text</div>";
+}
+
--- login.php
+++ login.php
@@ -1,1 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MealWorld</title>
+    <link rel="stylesheet" href="Stylesheets/login.css">
+</head>
+<body>
+<div id="login-page">
+    <!-- Login form section -->
+    <div class="login-border">
+        <form id="loginSection" action="login.php" method="POST">
+            <h1 id="loginTitle">Login</h1>
+            <p id="loginText">Please fill in your credentials to login.</p>
+            <input id="username-box" class="loginPageArea" type="text" name="username" placeholder="Username"
+                   value="<?php echo $_SESSION['username'] ?? ''; ?>">
+            <br>
+            <input id="password-box" class="loginPageArea" type="password" name="password" placeholder="Password">
+            <br>
+            <!-- Link to sign up page -->
+            <p>Need to create an account? <a href="signUp.php">Sign up now</a>.</p>
+            <br>
+            <?php echo $output; ?>
+            <!-- Login button -->
+            <input id="loginButton" type="submit" name="login" value="Login">
+        </form>
+    </div>
+</div>
+</body>
+</html>
+
+
+
